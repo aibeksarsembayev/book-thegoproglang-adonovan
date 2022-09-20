121b600a42b02d95a3cfb75ec2b321f9d9fd857c
quazar2000 <s-bek-k@mail.ru>
2022-09-20 22:02:09 +0600

    added tempconv, cf

---
+++ ch2/tempconv/conv.go
@@ -0,0 +1,11 @@
+package tempconv
+
+// CToF converts a Celsius temperature to Fahrenheit.
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// FToC converts a Fahrenheit temperature to Celsius.
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// exercise 2.1
+// CToK converts a Celsiuc temperature to Kelvin.
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) } 
